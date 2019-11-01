422f4e2791c1a32f214c9c56230a37cd00a6d8eb
taruc <reecheeteo@gmail.com>
2019-11-01 16:48:20 +0800

    new comment1

--- app/src/main/java/com/example/week2/MainActivity.kt
+++ app/src/main/java/com/example/week2/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //ttt
+        //tttt
         setContentView(R.layout.activity_main)
     }
 }
